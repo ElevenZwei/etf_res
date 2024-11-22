f343dc236ed5724e65e103a1ab2ecc644d2fdf7d
elevenz <elevenz@outlook.com>
2024-11-22 16:38:59 +0800

    feat: datavis add year month input.

--- datavis/dsp_scripts/all_dsp.py
+++ datavis/dsp_scripts/all_dsp.py
@@ -9,20 +9,22 @@ import s2_dsp_intersect as s2
 import s3_plot_dsp_surf as s3
 import s4_plot_dsp_inter as s4
 
-def all_dsp(spot: str, suffix: str, refresh: bool):
+def all_dsp(spot: str, suffix: str, refresh: bool, year: int, month: int):
     if refresh:
-        s0.auto_dl(spot, md_date=suffix)
+        s0.auto_dl(spot, year=year, month=month, md_date=suffix)
     s1.main(spot=spot, suffix=suffix)
     s2.intersect_merge_files(spot, suffix)
     s3.main(spot, suffix)
     s4.main(spot, suffix)
 
 @click.command()
-@click.option('-s', '--spot', type=str, help="spot code: 159915 510050")
-@click.option('-d', '--suffix', type=str, help="csv file name suffix.")
+@click.option('-s', '--spot', type=str, required=True, help="spot code: 159915 510050")
+@click.option('-d', '--date', type=str, help="format is %Y%m%d")
 @click.option('-r', '--refresh', is_flag=True, default=False, help="Download new data from database.")
-def click_main(spot: str, suffix: str, refresh: bool):
-    all_dsp(spot, suffix, refresh)
+@click.option('-y', '--year', type=int)
+@click.option('-m', '--month', type=int)
+def click_main(spot: str, date: str, refresh: bool, year: int, month: int):
+    all_dsp(spot, date, refresh, year, month)
 
 if __name__ == '__main__':
     click_main()

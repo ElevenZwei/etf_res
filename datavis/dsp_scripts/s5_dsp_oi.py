313205cbd7af7179d18342cd99387f6f30364a5b
elevenz <elevenz@outlook.com>
2024-12-26 19:04:02 +0800

    draft: dsp oi.

---
+++ datavis/dsp_scripts/s5_dsp_oi.py
@@ -0,0 +1,8 @@
+"""
+Open Interest Surface Calculation
+Simplified to the following steps:
+1. Use a filter on time axis.
+2. Intepolate on strike axis.
+3. Use different gaussian sigmas alongside the spot price.
+
+"""

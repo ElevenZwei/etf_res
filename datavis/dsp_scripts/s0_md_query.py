a9a663e488432ee165546a8abc3251bfa7afd500
elevenz <elevenz@outlook.com>
2025-04-07 14:50:48 +0800

    feat: faster download.

--- datavis/dsp_scripts/s0_md_query.py
+++ datavis/dsp_scripts/s0_md_query.py
@@ -1,13 +1,28 @@
 # 从 market_data_tick 数据表里面查询 OI 数据并且储存在 csv 文件里。
 
 from typing import Optional
+from collections import deque
 import click
 import datetime
 from dateutil.relativedelta import relativedelta
+import io
+import os
 import sqlalchemy
 import pandas as pd
 
 from dsp_config import DATA_DIR, PG_DB_CONF, gen_suffix
+
+def read_last_row(csv_path: str, encoding: str = "utf-8") -> pd.Series:
+    """
+    高效读取 CSV 文件的最后一行（包括 header 解析）。
+    适合大文件。
+    """
+    with open(csv_path, "r", encoding=encoding) as f:
+        header = next(f)                          # 第一行是列名
+        last_line = deque(f, maxlen=1)[0]         # 只保留最后一行
+    csv_fragment = header + last_line
+    df = pd.read_csv(io.StringIO(csv_fragment))
+    return df.iloc[0]  # 返回 Series 类型的一行
 
 def get_engine():
     return sqlalchemy.create_engine(sqlalchemy.URL.create(
@@ -37,12 +52,14 @@ def dl_expiry_date(spot: str, year: int, month: int) -> Optional[datetime.date]:
 
 def dl_oi_data(spot: str, expiry_date: datetime.date,
         bg_date: datetime.date, ed_date: datetime.date,
-        minute_bar: bool):
+        bg_time: datetime.time = datetime.time(9, 30, 0),
+        ed_time: datetime.time = datetime.time(15, 0, 0),
+        minute_bar: bool = False) -> pd.DataFrame:
     expiry_date_str = expiry_date.strftime('%Y-%m-%d')
     bg_date_str = bg_date.strftime('%Y-%m-%d')
     ed_date_str = ed_date.strftime('%Y-%m-%d')
-    bg_time = '09:30:00'
-    ed_time = '15:30:00'
+    bg_time_str = bg_time.strftime('%H:%M:%S')
+    ed_time_str = ed_time.strftime('%H:%M:%S')
     if minute_bar:
         if spot == '159915':
             suffix = '.SZ'
@@ -60,20 +77,21 @@ def dl_oi_data(spot: str, expiry_date: datetime.date,
                 select
                 dt, code, tradecode,
                 spotcode, expirydate, strike, callput,
-                openinterest as oi,
-                first_value(openinterest) over (partition by code order by dt) as oi_open
+                openinterest as oi
+                -- , first_value(openinterest) over (partition by code order by dt) as oi_open
                 from market_data dt join syms using(code)
-                where dt > '{bg_date_str} {bg_time}' and dt < '{ed_date_str} {ed_time}'),
+                where dt > '{bg_date_str} {bg_time_str}' and dt < '{ed_date_str} {ed_time_str}'),
             opt_oi as (
-                select dt, spotcode, expirydate, strike,
-                om1.tradecode as oi1c, om2.tradecode as oi2c,
-                om1.oi_open as oi_open_c, om2.oi_open as oi_open_p,
-                om1.oi - om1.oi_open as oi_diff_c, om2.oi - om2.oi_open as oi_diff_p
+                select dt, spotcode, expirydate, strike
+                , om1.tradecode as oi1c, om2.tradecode as oi2c
+                , om1.oi as oi_c, om2.oi as oi_p
+                -- , om1.oi_open as oi_open_c, om2.oi_open as oi_open_p
+                -- , om1.oi - om1.oi_open as oi_diff_c, om2.oi - om2.oi_open as oi_diff_p
                 from opt_mdt om1 join opt_mdt om2 using(dt, spotcode, expirydate, strike)
                 where om1.callput = 1 and om2.callput = -1)
             select opt_oi.*, md.closep as spot_price
             from opt_oi join market_data md using(dt)
-            where dt > '{bg_date_str} {bg_time}' and dt < '{ed_date_str} {ed_time}'
+            where dt > '{bg_date_str} {bg_time_str}' and dt < '{ed_date_str} {ed_time_str}'
             and md.code = opt_oi.spotcode
             order by dt asc;
         """
@@ -85,37 +103,39 @@ def dl_oi_data(spot: str, expiry_date: datetime.date,
         query = f"""
             set enable_nestloop=false;
             with OI as (
-                select *, oi - oi_open as oi_diff
+                select *
+                -- , oi - oi_open as oi_diff
                 -- , log(oi) - log(oi_open) as oi_dlog
                 from (
                     select
                         dt, spotcode, expirydate, callput, strike, tradecode,
-                        open_interest as oi,
-                        first_value(open_interest) over (partition by code order by dt) as oi_open
+                        open_interest as oi
+                        -- , first_value(open_interest) over (partition by code order by dt) as oi_open
                     from market_data_tick mdt join contract_info ci using(code)
-                    where dt >= '{f"{bg_date_str} {bg_time}"}' and dt <= '{f"{ed_date_str} {ed_time}"}'
+                    where dt >= '{f"{bg_date_str} {bg_time_str}"}' and dt <= '{f"{ed_date_str} {ed_time_str}"}'
                     and dt::time >= '09:30:00' and dt::time <= '15:00:00'
                     and spotcode = '{spot}' and expirydate = '{expiry_date_str}'
                 ) as T
             ),
             OD as (
                 select
-                    oi1.dt, spotcode, expirydate, strike,
-                    oi1.tradecode as oi1c, oi2.tradecode as oi2c,
-                    oi1.oi_open as oi_open_c, oi2.oi_open as oi_open_p,
-                    oi1.oi_diff as oi_diff_c, oi2.oi_diff as oi_diff_p
+                    oi1.dt, spotcode, expirydate, strike
+                    , oi1.tradecode as oi1c, oi2.tradecode as oi2c
+                    , oi1.oi as oi_c, oi2.oi as oi_p
+                    -- , oi1.oi_open as oi_open_c, oi2.oi_open as oi_open_p
+                    -- , oi1.oi_diff as oi_diff_c, oi2.oi_diff as oi_diff_p
                     -- , oi1.oi_dlog as oi_dlog_c, oi2.oi_dlog as oi_dlog_p
                 from OI oi1 join OI oi2 using (dt, spotcode, expirydate, strike)
                 where oi1.callput = 1 and oi2.callput = -1
             )
             select od.*, mdt.last_price as spot_price
             from OD join market_data_tick mdt using (dt)
-            where dt >= '{f"{bg_date_str} {bg_time}"}' and dt <= '{f"{ed_date_str} {ed_time}"}'
+            where dt >= '{f"{bg_date_str} {bg_time_str}"}' and dt <= '{f"{ed_date_str} {ed_time_str}"}'
             and mdt.code = od.spotcode
             order by dt asc;
         """
     
-    # print(query)
+    print(query)
     with get_engine().connect() as conn:
         df = pd.read_sql(query, conn)
     
@@ -125,18 +145,56 @@ def dl_oi_data(spot: str, expiry_date: datetime.date,
     df['dt'] = df['dt'].dt.tz_convert('Asia/Shanghai')
     df['dt'] = df['dt'].dt.strftime('%Y-%m-%dT%H:%M:%S%z')
     return df
+    
+def df_calc_open_diff(df: pd.DataFrame) -> pd.DataFrame:
+    call_df = df.pivot(index='dt', columns='strike', values='oi_c')
+    call_df = call_df.ffill().bfill()
+    call_df = call_df - call_df.iloc[0]
+    call_melt = call_df.melt(ignore_index=False, var_name='strike', value_name='oi_diff_c')
+    put_df = df.pivot(index='dt', columns='strike', values='oi_p')
+    put_df = put_df.ffill().bfill()
+    put_df = put_df - put_df.iloc[0]
+    put_melt = put_df.melt(ignore_index=False, var_name='strike', value_name='oi_diff_p')
+    df2 = pd.merge(call_melt, put_melt, left_on=['dt', 'strike'], right_on=['dt', 'strike'], how='inner')
+    # print(df2)
+    # exit(1)
+    df = pd.merge(df, df2, left_on=['dt', 'strike'], right_on=['dt', 'strike'], how='inner')
+    return df
 
 def dl_save_range_oi(spot: str, expiry_date: datetime.date,
         bg_date: datetime.date, ed_date: datetime.date,
         minute_bar: bool):
-    df = dl_oi_data(spot, expiry_date, bg_date, ed_date, minute_bar=minute_bar)
     bg_date_str = bg_date.strftime('%Y%m%d')
     ed_date_str = ed_date.strftime('%Y%m%d')
     expiry_date_str = expiry_date.strftime('%Y%m%d')
     date_suffix = bg_date_str if bg_date == ed_date else f'{bg_date_str}_{ed_date_str}'
     suffix = gen_suffix(expiry_date_str, date_suffix)
-    fname = f'strike_oi_diff_{spot}_{suffix}.csv'
-    df.to_csv(f'{DATA_DIR}/dsp_input/{fname}', index=False)
+    fname = f'strike_oi_raw_{spot}_{suffix}.csv'
+    fpath = f'{DATA_DIR}/dsp_input/{fname}'
+
+    if os.path.exists(fpath):
+        last_row = read_last_row(fpath)
+        last_dt = pd.to_datetime(last_row['dt'])
+        bg_date = last_dt.date()
+        bg_time = (last_dt + datetime.timedelta(seconds=1)).time()
+    else:
+        bg_time = datetime.time(9, 30, 0)
+    df2 = dl_oi_data(spot, expiry_date,
+            bg_date, ed_date,
+            bg_time=bg_time, ed_time=datetime.time(15, 0, 0),
+            minute_bar=minute_bar)
+
+    if os.path.exists(fpath):
+        df1 = pd.read_csv(fpath)
+        df = pd.concat([df1, df2], ignore_index=True)
+    else:
+        df = df2
+    df.to_csv(fpath, index=False)
+
+    df_diff = df_calc_open_diff(df)
+    fname_diff = f'strike_oi_diff_{spot}_{suffix}.csv'
+    fpath_diff = f'{DATA_DIR}/dsp_input/{fname_diff}'
+    df_diff.to_csv(fpath_diff, index=False)
     return suffix
 
 def get_nearest_expirydate(spot: str, dt: datetime.datetime):

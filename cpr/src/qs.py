a1d5c945b6cea84cb4c9ba5656c05387d06ceecc
elevenz <elevenz@outlook.com>
2025-09-19 15:53:54 +0800

    feat: add qs report.

---
+++ cpr/src/qs.py
@@ -0,0 +1,6 @@
+import quantstats as qs
+import pandas as pd
+
+rp = pd.read_csv('../notes/rolling_159915_profit.csv', index_col=0, parse_dates=True)
+rp['profit'] = rp['profit_percent_avg']
+qs.reports.html(rp['profit'], output='qs_report.html', title='QuantStats Report')

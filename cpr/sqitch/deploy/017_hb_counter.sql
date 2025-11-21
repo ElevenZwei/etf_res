709e2c42e84905ce6648c527a2d2a272026c0db5
elevenz <elevenz@outlook.com>
2025-11-21 17:31:39 +0800

    fix: alter hb_counter table.

--- cpr/sqitch/deploy/017_hb_counter.sql
+++ cpr/sqitch/deploy/017_hb_counter.sql
@@ -6,13 +6,15 @@ BEGIN;
 create table if not exists hb.counter (
     id serial primary key,
     username text not null,
-    config_name text not null,
+    config_name text,
     counter_name text not null,
+    dt timestamptz not null default now(),
     value int not null,
     updated_at timestamptz not null default now()
 );
 
-create index if not exists idx_hb_counter_username_time_config_counter
-    on hb.counter (username, updated_at, config_name, counter_name);
+-- 不要在这个表格上定义唯一约束，这里接受并发写入，允许覆盖更新。
+create index if not exists idx_hb_counter_username_dt_counter_config
+    on hb.counter (username, dt, counter_name, config_name);
 
 COMMIT;

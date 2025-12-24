5dc91cbb737660c18f74b0a2a49f3dc5a0de660e
elevenz <elevenz@outlook.com>
2025-12-24 14:59:34 +0800

    fix: update_quantstats profit percent.

--- cpr/src/update_quantstats.py
+++ cpr/src/update_quantstats.py
@@ -6,6 +6,7 @@ import pandas as pd
 # qs.reports.html(rp['profit'], output='../notes/rolling_159915_profit_report.html', title='QuantStats Report')
 
 rp = pd.read_csv('../data/sig_worth/roll_worth_daily.csv', index_col=0, parse_dates=True)
-rp['profit'] = rp['net_1_daily'].diff().fillna(rp['net_1_daily'])
-print(rp)
+rp['profit'] = (1 + rp['net_1_daily']).pct_change().fillna(rp['net_1_daily'])
+rp['profit_accum'] = (1 + rp['profit']).cumprod() - 1
+print(rp[['net_1_daily', 'profit_accum', 'profit']].tail())
 qs.reports.html(rp['profit'], output='../notes/roll_worth_daily_report.html', title='QuantStats Report')

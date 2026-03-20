100c64dcdf365f63f2a9c1a14f81efe3696f8aeb
elevenz <elevenz@outlook.com>
2026-03-20 17:31:19 +0800

    feat: onetime scripts.

---
+++ cpr/src/onetime/import_2024.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from pathlib import Path
+from datetime import datetime, date
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from config import DATA_DIR
+from weekly_update import load_data, clip_data, backtest_data, roll_data
+
+# for month in range(12, 13):
+#     dt_from = date(2024, month, 1)
+#     if month == 12:
+#         dt_to = date(2025, 1, 1)
+#     else:
+#         dt_to = date(2024, month + 1, 1)
+#     load_data('159915', dt_from, dt_to)
+
+# clip_data('159915', date(2025, 1, 1), date(2025, 5, 1))
+
+
+# backtest_data('159915', date(2024, 5, 1), date(2025, 1, 1))
+# backtest_data('159915', date(2024, 1, 1), date(2024, 7, 1))
+
+# clip_data('159915', date(2023, 5, 1), date(2023, 7, 1))
+# backtest_data('159915', date(2023, 9, 1), date(2024, 1, 1))
+# roll_data('159915', date(2024, 1, 1), date(2024, 7, 1),
+#         with_roll_next=True, with_roll_export=False)
+
+# backtest_data('159915', date(2025, 5, 1), date(2025, 6, 1))
+# roll_data('159915', date(2025, 1, 1), date(2025, 6, 1),
+#         with_roll_next=True, with_roll_export=False)
+
